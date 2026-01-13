ecbe09761a5e4a29071d0c4d6af2af70c554cc25
Mason Silber <mason@anthropic.com>
2026-01-13 20:38:51 +0000

    Add configuration module

---
+++ config.py
@@ -0,0 +1,11 @@
+"""Configuration management."""
+
+DEFAULT_PORT = 8080
+DEFAULT_HOST = "localhost"
+
+def get_config():
+    return {
+        "port": DEFAULT_PORT,
+        "host": DEFAULT_HOST,
+    }
+# comment from config branch
